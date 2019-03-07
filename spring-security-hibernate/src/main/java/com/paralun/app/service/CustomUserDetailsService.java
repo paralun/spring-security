4b28394521a96b099f8d7b1c3112148126113813
James Kusmambang <kusmambang@gmail.com>
2019-03-07 16:39:17 +0700

    add service

---
+++ spring-security-hibernate/src/main/java/com/paralun/app/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.paralun.app.service;
+
+import com.paralun.app.dao.UserDao;
+import com.paralun.app.model.UserRole;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    @Autowired
+    private UserDao dao;
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.paralun.app.model.User user = dao.findByUserName(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        
+        return new User(user.getUsername(), user.getPassword(), user.getState().equals("Active"), true, true, true, getGrantedAuthorities(user));
+    }
+    
+    private List<GrantedAuthority> getGrantedAuthorities(com.paralun.app.model.User user) {
+        List<GrantedAuthority> authoritys = new VirtualFlow.ArrayLinkedList<>();
+        for(UserRole role : user.getRoles()) {
+            authoritys.add(new SimpleGrantedAuthority("ROLE_" + role.getType()));
+        }
+        
+        return authoritys;
+    }
+    
+}
