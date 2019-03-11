7f7d4f8fb8dd7fd243ffa374b6b45adb19dc83a0
James Kusmambang <kusmambang@gmail.com>
2019-03-11 17:33:21 +0700

    add jpa configuration

---
+++ spring-security-crud/src/main/java/com/paralun/app/config/JpaConfiguration.java
@@ -0,0 +1,65 @@
+package com.paralun.app.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.paralun.repo", entityManagerFactoryRef = "factoryBean")
+public class JpaConfiguration {
+    
+    @Autowired
+    private Environment env;
+    
+    @Bean("factoryBean")
+    public LocalContainerEntityManagerFactoryBean factoryBean() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setJpaVendorAdapter(vendorAdapter());
+        factory.setPackagesToScan("com.paralun.app.model");
+        return factory;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(env.getProperty("jdbc.driver"));
+        config.setJdbcUrl(env.getProperty("jdbc.url"));
+        config.setUsername(env.getProperty("jdbc.username"));
+        config.setPassword(env.getProperty("jdbc.password"));
+        config.setMaximumPoolSize(10);
+        return new HikariDataSource(config);
+    }
+    
+    @Bean
+    public JpaVendorAdapter vendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setGenerateDdl(true);
+        adapter.setShowSql(true);
+        return adapter;
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(factoryBean().getObject());
+        manager.setJpaDialect(new HibernateJpaDialect());
+        return manager;
+    }
+    
+}
