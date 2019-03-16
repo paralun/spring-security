0cd7578d9191e731aacd02ece30853ff9b3458c0
James Kusmambang <kusmambang@gmail.com>
2019-03-16 16:36:21 +0700

    add views

--- spring-boot-security-db/src/main/java/com/paralun/app/controller/AppController.java
+++ spring-boot-security-db/src/main/java/com/paralun/app/controller/AppController.java
@@ -43,7 +43,7 @@ public class AppController {
             userService.saveUser(user);
             model.addAttribute("successMessage", "User has been registered successfully");
             model.addAttribute("user", new User());
-            return "registration";
+            return "redirect:/login";
         }
     }
 
