55dc9096f7b2ed9fe725cec0a469aad6787abcbe
James Kusmambang <kusmambang@gmail.com>
2019-03-28 16:12:27 +0700

    initial

---
+++ spring-boot-preauthorize/src/main/java/com/paralun/app/SpringBootPreauthorizeApplication.java
@@ -0,0 +1,13 @@
+package com.paralun.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPreauthorizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPreauthorizeApplication.class, args);
+    }
+
+}
